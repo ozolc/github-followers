7f1110b8e81f37e3da13bd718b94dea5ba83391c
Maksim Nosov <ozolc@yandex.ru>
2020-01-17 14:46:44 +0200

    1. Getting Started - 3. Initial Project Setup - No Storyboard

---
+++ GHFolowers/GHFolowers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GHFolowers
+//
+//  Created by Maksim Nosov on 17.01.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
