3f729014129a62488b44de65f1d73e22c659b886
Maksim Nosov <ozolc@yandex.ru>
2020-01-20 11:29:18 +0200

    3. Networking - 1. Planning our Network Calls - API, JSON, Model

---
+++ GHFolowers/GHFolowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFolowers
+//
+//  Created by Maksim Nosov on 20.01.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
