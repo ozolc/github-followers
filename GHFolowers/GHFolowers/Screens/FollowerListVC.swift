2ac69bb1b8a8de30bdb17e62505ca02ac549f402
Maksim Nosov <ozolc@yandex.ru>
2020-01-24 13:12:52 +0200

    4. CollectionView - 5. ARC, Memory Leaks, and Capture Lists [weak self]

--- GHFolowers/GHFolowers/Screens/FollowerListVC.swift
+++ GHFolowers/GHFolowers/Screens/FollowerListVC.swift
@@ -34,22 +34,9 @@ class FollowerListVC: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumSpacing: CGFloat = 10
-        let availableWidth = width - (2 * padding) - (2 * minimumSpacing)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
-    
     func getFollowers () {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
             
             switch result {
             case .success(let followers):
@@ -68,7 +55,7 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
---
+++ GHFolowers/GHFolowers/Utilities/UIHelper.swift
@@ -0,0 +1,25 @@
+//
+//  UIHelper.swift
+//  GHFolowers
+//
+//  Created by Maksim Nosov on 24.01.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumSpacing: CGFloat = 10
+        let availableWidth = width - (2 * padding) - (2 * minimumSpacing)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
