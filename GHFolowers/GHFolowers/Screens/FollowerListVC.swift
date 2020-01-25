90bff797cbbd4519f12eaf27402652b9060e1014
Maksim Nosov <ozolc@yandex.ru>
2020-01-25 18:06:56 +0200

    4. CollectionView - 09. Empty State View

--- GHFolowers/GHFolowers/Screens/FollowerListVC.swift
+++ GHFolowers/GHFolowers/Screens/FollowerListVC.swift
@@ -18,7 +18,7 @@ class FollowerListVC: UIViewController {
     var followers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
-
+    
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
@@ -46,6 +46,16 @@ class FollowerListVC: UIViewController {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😃."
+                    
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    
+                    return
+                }
                 self.updateData()
                 
             case .failure(let error):
