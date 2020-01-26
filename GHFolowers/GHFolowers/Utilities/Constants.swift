68c0dc303632958157d80e8708e0a635b761e2b2
Maksim Nosov <ozolc@yandex.ru>
2020-01-26 15:18:38 +0200

    5. User Info Screen - 4. Child View Controller - UserInfoHeaderVC

---
+++ GHFolowers/GHFolowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFolowers
+//
+//  Created by Maksim Nosov on 26.01.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
