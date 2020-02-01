37c1b408dad08fb45dffa21993b6c90927ecac88
Maksim Nosov <ozolc@yandex.ru>
2020-02-01 18:55:37 +0200

    7. Clean Up & Optimization - 01. Optimization Part 1

---
+++ GHFolowers/GHFolowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBarController.swift
+//  GHFolowers
+//
+//  Created by Maksim Nosov on 01.02.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+    
+}
--- GHFolowers/GHFolowers/Screens/FollowerListVC.swift
+++ GHFolowers/GHFolowers/Screens/FollowerListVC.swift
@@ -28,6 +28,12 @@ class FollowerListVC: UIViewController {
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
+    init(username: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.username = username
+        title = username
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -143,6 +149,10 @@ class FollowerListVC: UIViewController {
             }
         }
         
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 }
